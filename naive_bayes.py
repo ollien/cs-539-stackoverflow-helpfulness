34f422dd5ddbfef294bc80d9a5631c4d7c8f67d7
Nick Krichevsky <nick@ollien.com>
2020-11-18 20:22:19 -0500

    Add basic naive bayes

---
+++ naive_bayes.py
@@ -0,0 +1,45 @@
+import click
+import pandas
+import sklearn.feature_extraction.text
+import sklearn.naive_bayes
+import sklearn.metrics
+import nltk
+import numpy
+import math
+
+
+def get_file_data(filename: str):
+    all_data = pandas.read_csv(filename)
+    return all_data[["BodyCleaned", "Y"]]
+
+
+def vectorize_data(data):
+    stop_words = nltk.corpus.stopwords.words("english")
+    vectorizer = sklearn.feature_extraction.text.CountVectorizer(stop_words=stop_words)
+    counts = vectorizer.fit_transform(data)
+    transformer = sklearn.feature_extraction.text.TfidfTransformer().fit(counts)
+    return transformer.transform(counts)
+
+def nltk_download():
+    try:
+        nltk.data.find("corpora/stopwords")
+    except LookupError:
+        nltk.download("stopwords")
+
+@click.command()
+@click.option("--train_file", required=True)
+@click.option("--test_file", required=True)
+def main(train_file: str, test_file: str):
+    nltk_download()
+    training_data = get_file_data(train_file)
+    test_data = get_file_data(train_file)
+    res = vectorize_data(training_data["BodyCleaned"])
+    model = sklearn.naive_bayes.MultinomialNB()
+    model.fit(res, training_data["Y"])
+    vectorized_test_data = vectorize_data(training_data["BodyCleaned"])
+    predicted = model.predict(vectorized_test_data)
+    print(sklearn.metrics.accuracy_score(training_data["Y"], predicted))
+
+
+if __name__ == "__main__":
+    main()
