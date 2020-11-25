3fe96a29bd5cbd6dcf5b3e7ebb24cbf5e7d088ca
Nick Krichevsky <nick@ollien.com>
2020-11-24 22:55:29 -0500

    Fix naive bayes
    68% accuracy :(

--- naive_bayes.py
+++ naive_bayes.py
@@ -18,7 +18,8 @@ def vectorize_data(data):
     vectorizer = sklearn.feature_extraction.text.CountVectorizer(stop_words=stop_words)
     counts = vectorizer.fit_transform(data)
     transformer = sklearn.feature_extraction.text.TfidfTransformer().fit(counts)
-    return transformer.transform(counts)
+    return vectorizer, transformer, transformer.transform(counts)
+
 
 def nltk_download():
     try:
@@ -26,19 +27,20 @@ def nltk_download():
     except LookupError:
         nltk.download("stopwords")
 
+
 @click.command()
 @click.option("--train_file", required=True)
 @click.option("--test_file", required=True)
 def main(train_file: str, test_file: str):
     nltk_download()
     training_data = get_file_data(train_file)
-    test_data = get_file_data(train_file)
-    res = vectorize_data(training_data["BodyCleaned"])
+    test_data = get_file_data(test_file)
+    vectorizer, transformer, res = vectorize_data(training_data["BodyCleaned"])
     model = sklearn.naive_bayes.MultinomialNB()
     model.fit(res, training_data["Y"])
-    vectorized_test_data = vectorize_data(training_data["BodyCleaned"])
+    vectorized_test_data = transformer.transform(vectorizer.transform(test_data["BodyCleaned"]))
     predicted = model.predict(vectorized_test_data)
-    print(sklearn.metrics.accuracy_score(training_data["Y"], predicted))
+    print(sklearn.metrics.accuracy_score(test_data["Y"], predicted))
 
 
 if __name__ == "__main__":
