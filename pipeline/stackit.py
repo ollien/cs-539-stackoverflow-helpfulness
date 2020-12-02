f7b8c015ce7cbd6eef8dd826064acf8b297165cc
Nick Krichevsky <nick@ollien.com>
2020-12-01 22:09:11 -0500

    Adjust to 89.4% accuracy

--- pipeline/stackit.py
+++ pipeline/stackit.py
@@ -1,6 +1,6 @@
 import pandas
 import numpy
-import sklearn, sklearn.model_selection, sklearn.linear_model, sklearn.metrics
+import sklearn, sklearn.model_selection, sklearn.linear_model, sklearn.metrics, sklearn.svm
 
 
 def main(nn_file_path: str, other_classifier_file: str):
@@ -18,12 +18,12 @@ def main(nn_file_path: str, other_classifier_file: str):
         sklearn_file.loc[sklearn_file.num == i, "nn"] = prediction
 
     print("Training...")
-    logistic = sklearn.linear_model.LogisticRegression()
+    logistic = sklearn.svm.SVC(C=4, kernel="rbf")
     crossval_scores = sklearn.model_selection.cross_validate(
         logistic,
         sklearn_file[["lr", "cart", "svc", "knn", "nn"]],
         sklearn_file["Y"],
-        cv=10,
+        cv=5,
         scoring="accuracy",
     )
 
